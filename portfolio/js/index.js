1144e37476b3e374952e70e920ac1cc422ca01c3
Dmitry Beresnev <dmitrberesnev@gmail.com>
2022-01-30 14:29:30 +0300

    feat: add switch theme

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -3,16 +3,45 @@ import i18Obj from './translate.js';
 const body = document.body;
 const adaptiveMenu = document.querySelector('.header_menu');
 const navLinks = document.querySelector('.menu_list');
+const themeSwith = document.querySelector('.switch_theme');
+const langSwitch = document.querySelector('.switch_lang');
 const hamburgerButton = document.querySelector('.hamburger');
-const langsBox = document.querySelector('.switch_lang');
 const portfolioMenu = document.querySelector('.portfolio_menu');
+const langButton = document.querySelectorAll('.lang_button');
 const portfolioPhoto = document.querySelectorAll('.portfolio_photo');
 const portfolioButton = document.querySelectorAll('.portfolio_button');
+const sectionTitle = document.querySelectorAll('.section_title');
+const buttons = document.querySelectorAll('.button');
+const message = document.querySelectorAll('.message');
+const icon = document.querySelectorAll('.icon');
 
-hamburgerButton.addEventListener('click', menuOpenAndClose);
+const lightThemeArray = [
+    body,
+    adaptiveMenu,
+    document.querySelector('.header_container'),
+    document.querySelector('.hero_container'),
+    document.querySelector('.contact_container'),
+    document.querySelector('.contact_title'),
+    document.querySelector('.contact_content')
+];
+
+icon.forEach( (current) => lightThemeArray.push(current));
+buttons.forEach( (current) => lightThemeArray.push(current));
+sectionTitle.forEach( (current) => lightThemeArray.push(current));
+langButton.forEach( (current) => lightThemeArray.push(current));
+message.forEach( (current) => lightThemeArray.push(current));
+
+langSwitch.addEventListener('click', changeLang);
+themeSwith.addEventListener('click', changeTheme);
 navLinks.addEventListener('click', closeMenu);
+hamburgerButton.addEventListener('click', menuOpenAndClose);
 portfolioMenu.addEventListener('click', switchSeason);
-langsBox.addEventListener('click', switchLang);
+
+function changeTheme() {
+    lightThemeArray.forEach( (current) => {
+        current.classList.toggle('light_theme');
+    })
+}
 
 function getTranslate(lang) {
     document.querySelectorAll('[data-i18]').forEach( (current) => {
@@ -25,9 +54,9 @@ function getTranslate(lang) {
     })
 }
 
-function switchLang(event) {
+function changeLang(event) {
     if (event.target.classList.contains('lang_button')) {
-        langsBox.querySelectorAll('.lang_button').forEach( (current) => {
+        langSwitch.querySelectorAll('.lang_button').forEach( (current) => {
             current.classList.remove('lang_active');
         })
 
@@ -49,6 +78,7 @@ function switchSeason(event) {
 
 function menuOpenAndClose () {
     hamburgerButton.classList.toggle('is_active');
+    body.classList.toggle('is_active');
     adaptiveMenu.classList.toggle('is_open');
     body.classList.toggle('fixed_scroll');
 }
@@ -56,6 +86,7 @@ function menuOpenAndClose () {
 function closeMenu (event) {
     if ( event.target.classList.contains('menu_link') ) {
         hamburgerButton.classList.remove('is_active');
+        body.classList.remove('is_active');
         adaptiveMenu.classList.remove('is_open');
         body.classList.remove('fixed_scroll');
     }
